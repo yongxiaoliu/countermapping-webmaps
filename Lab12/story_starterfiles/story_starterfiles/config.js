bc6a3e412536e860c1f8dd13073649153923f554
Yongxiao Liu <yl3723@cornell.edu>
2024-11-17 17:25:07 -0500

    add

---
+++ Lab12/story_starterfiles/story_starterfiles/config.js
@@ -0,0 +1,87 @@
+var config = {
+    style: 'mapbox://styles/yongxiao/cm2xi4y60000701pf5kaw0rm3',
+    accessToken: 'pk.eyJ1IjoieW9uZ3hpYW8iLCJhIjoiY20xdGo0NTY2MDJ3cTJxcHJ3OW82YjZteiJ9.65aI1m2ArWDTjob1ilSsAA',
+    showMarkers: true,
+    markerColor: '#3FB1CE',
+    projection: 'equirectangular',
+    //Read more about available projections here
+    //https://docs.mapbox.com/mapbox-gl-js/example/projections/
+    inset: false,
+    theme: 'light',
+    use3dTerrain: true, //set true for enabling 3D maps.
+    title: 'NYC Housing Flood Risk',
+    subtitle: 'Will Happen Within 100 Years',
+    byline: 'By Yongxiao Liu',
+    para1:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    para2:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    chapters: [
+        {
+            id: 'slug-style-id',
+            alignment: 'left',
+            hidden: false,
+            title: 'Predicted Floodplains',
+            description: 'In the fall of 2012, New York City was hit with hurricane that left tens of thousands of public housing residents without power for days or even weeks. New York City Housing Authority public housing developments were affected. 24,000 government-subsidized apartments and 40,000 rent-stabilized apartments were also affected. With rising sea levels from climate change and the promise of more flooding, which areas and who will likely be most affected? How is affordable housing construction responding to this?',
+            location: {
+                center: [-74.00993, 40.70637],
+                zoom: 12,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'interlude',
+            alignment: 'center',
+            hidden: false,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultricies tempor purus, eget tempus sem bibendum vel. Maecenas at justo non felis accumsan condimentum vitae et leo. Sed sodales nec risus maximus congue. Ut quis tincidunt leo. Pellentesque sapien nisl, congue tempus mauris in, pulvinar mattis turpis. Nullam sit amet justo egestas, laoreet mauris ut, pulvinar ipsum. Pellentesque vehicula imperdiet iaculis. Nam in tellus metus. Donec a est non orci efficitur ornare id mattis eros. Aliquam vel facilisis ligula. Nullam tincidunt, sapien vitae bibendum consequat, nibh eros eleifend ligula, et aliquam enim justo eget felis. Sed nec vehicula metus. Pellentesque rutrum dui quis egestas hendrerit. Nullam auctor et tortor sit amet congue.',
+            video: 'images/902-1_902-2364-PD2_preview.mp4',
+            location: {
+            center: [-73.97615, 40.71901],
+            zoom: 14.84,
+            pitch: 48,
+            bearing: 142.44,
+    },
+},
+        {
+            id: 'second-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: 'Flood-related Damages to the Red Hook Houses',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/6/64/Red_Hook_Project%2C_Brooklyn%2C_New_York._LOC_gsc.5a03936.jpg',
+            description: 'The city classifies Red Hook Houses as being in flood zone 1, indicating high flood risk. Before Hurricane Sandy in 2012, various city reports had outlined the need for increased flood protection, projecting floods as high as 5 feet during storm surges. NYCHA officials responded that they did not anticipate storm surges that strong. When Hurricane Sandy struck, the development was inundated with 6 feet (1.8 m) of flood water through sewer overflow and high tides. The storm left residents without power and fresh water for months. This caused leaks, resulting in mold that hurt many residents health.',
+            location: {
+                center: [-74.00468, 40.67564],
+                zoom: 19,
+                pitch: 60,
+                bearing: -43.2,
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+    ]
+};
